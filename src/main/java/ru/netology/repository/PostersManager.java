6bf712943603a9e4438fcbc11e1f184b35528e7e
Nov.s <novikov.su20@gmail.com>
2023-03-13 03:17:59 +0500

    Reformat code

--- src/main/java/ru/netology/repository/PostersManager.java
+++ src/main/java/ru/netology/repository/PostersManager.java
@@ -1,23 +1,25 @@
 package ru.netology.repository;
+
 import ru.netology.domain.Posters;
 
 public class PostersManager {
     private Posters[] postersLine = new Posters[0];
     private int limit;
 
-    public PostersManager (){
+    public PostersManager() {
         this.limit = 10;
     }
-    public PostersManager (int limit){
+
+    public PostersManager(int limit) {
         this.limit = limit;
     }
 
-    public void add(Posters poster){
+    public void add(Posters poster) {
         Posters[] tmp = new Posters[postersLine.length + 1];
-        for (int i = 0; i < postersLine.length; i++){
-            tmp[i]= postersLine[i];
+        for (int i = 0; i < postersLine.length; i++) {
+            tmp[i] = postersLine[i];
         }
-        tmp[tmp.length-1] = poster;
+        tmp[tmp.length - 1] = poster;
         postersLine = tmp;
     }
 
@@ -25,18 +27,17 @@ public class PostersManager {
         return postersLine;
     }
 
-    public Posters [] findLast(){
+    public Posters[] findLast() {
         int reversedLength;
         Posters[] all = findAll();
-        if(all.length < limit){
+        if (all.length < limit) {
             reversedLength = all.length;
-        }
-        else {
+        } else {
             reversedLength = limit;
         }
         Posters[] reversed = new Posters[reversedLength];
-        for (int i = 0; i < reversed.length; i++){
-            reversed [i] = all[all.length - 1 - i];
+        for (int i = 0; i < reversed.length; i++) {
+            reversed[i] = all[all.length - 1 - i];
         }
         return reversed;
     }
--- src/test/java/ru/netology/repository/PostersManagerTest.java
+++ src/test/java/ru/netology/repository/PostersManagerTest.java
@@ -8,21 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PostersManagerTest {
 
-    Posters film1 = new Posters(1,100,"Film 1");
-    Posters film2 = new Posters(2,101,"Film 2");
-    Posters film3 = new Posters(3,102,"Film 3");
-    Posters film4 = new Posters(4,103,"Film 4");
-    Posters film5 = new Posters(5,104,"Film 5");
-    Posters film6 = new Posters(6,105,"Film 6");
-    Posters film7 = new Posters(7,106,"Film 7");
-    Posters film8 = new Posters(8,107,"Film 8");
-    Posters film9 = new Posters(9,108,"Film 9");
-    Posters film10 = new Posters(10,109,"Film 10");
-    Posters film11 = new Posters(11,110,"Film 11");
+    Posters film1 = new Posters(1, 100, "Film 1");
+    Posters film2 = new Posters(2, 101, "Film 2");
+    Posters film3 = new Posters(3, 102, "Film 3");
+    Posters film4 = new Posters(4, 103, "Film 4");
+    Posters film5 = new Posters(5, 104, "Film 5");
+    Posters film6 = new Posters(6, 105, "Film 6");
+    Posters film7 = new Posters(7, 106, "Film 7");
+    Posters film8 = new Posters(8, 107, "Film 8");
+    Posters film9 = new Posters(9, 108, "Film 9");
+    Posters film10 = new Posters(10, 109, "Film 10");
+    Posters film11 = new Posters(11, 110, "Film 11");
     PostersManager manager = new PostersManager(10);
 
     @Test
-    public void testShouldFindAll(){
+    public void testShouldFindAll() {
         manager.add(film1);
         manager.add(film2);
         manager.add(film3);
@@ -34,31 +34,33 @@ class PostersManagerTest {
         manager.add(film9);
         manager.add(film10);
 
-        Posters[] expected = {film1,film2,film3,film4,film5,film6,film7,film8,film9,film10};
+        Posters[] expected = {film1, film2, film3, film4, film5, film6, film7, film8, film9, film10};
         Posters[] actual = manager.findAll();
 
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testShouldFindAllIfOne(){
+    public void testShouldFindAllIfOne() {
         manager.add(film1);
 
 
         Posters[] expected = {film1};
         Posters[] actual = manager.findAll();
 
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void testShouldFindAllIfNoFilms(){
+    public void testShouldFindAllIfNoFilms() {
         Posters[] expected = new Posters[0];
         Posters[] actual = manager.findAll();
 
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void testShouldFindLastIfAll(){
+    public void testShouldFindLastIfAll() {
         manager.add(film1);
         manager.add(film2);
         manager.add(film3);
@@ -70,14 +72,14 @@ class PostersManagerTest {
         manager.add(film9);
         manager.add(film10);
 
-        Posters[] expected = {film10,film9,film8,film7,film6,film5,film4,film3,film2,film1};
+        Posters[] expected = {film10, film9, film8, film7, film6, film5, film4, film3, film2, film1};
         Posters[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testShouldFindLastIfNotAll(){
+    public void testShouldFindLastIfNotAll() {
         manager.add(film1);
         manager.add(film2);
         manager.add(film3);
@@ -85,20 +87,21 @@ class PostersManagerTest {
         manager.add(film5);
 
 
-        Posters[] expected = {film5,film4,film3,film2,film1};
+        Posters[] expected = {film5, film4, film3, film2, film1};
         Posters[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testShouldFindLastBoundaryValuesNull(){
+    public void testShouldFindLastBoundaryValuesNull() {
 
         Posters[] expected = new Posters[0];
         Posters[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void testShouldFindLastBoundaryValuesOne() {
         manager.add(film1);
@@ -124,14 +127,14 @@ class PostersManagerTest {
         manager.add(film10);
         manager.add(film11);
 
-        Posters[] expected = {film11,film10, film9, film8, film7, film6, film5, film4, film3, film2};
+        Posters[] expected = {film11, film10, film9, film8, film7, film6, film5, film4, film3, film2};
         Posters[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testShouldSetNewLimit(){
+    public void testShouldSetNewLimit() {
         PostersManager manager = new PostersManager(11);
         manager.add(film1);
         manager.add(film2);
@@ -145,13 +148,14 @@ class PostersManagerTest {
         manager.add(film10);
         manager.add(film11);
 
-        Posters[] expected = {film11,film10, film9, film8, film7, film6, film5, film4, film3, film2,film1};
+        Posters[] expected = {film11, film10, film9, film8, film7, film6, film5, film4, film3, film2, film1};
         Posters[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void testShouldSetLimitByDefault(){
+    public void testShouldSetLimitByDefault() {
         PostersManager manager = new PostersManager();
         manager.add(film1);
         manager.add(film2);
@@ -165,7 +169,7 @@ class PostersManagerTest {
         manager.add(film10);
         manager.add(film11);
 
-        Posters[] expected = {film11,film10, film9, film8, film7, film6, film5, film4, film3, film2};
+        Posters[] expected = {film11, film10, film9, film8, film7, film6, film5, film4, film3, film2};
         Posters[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
